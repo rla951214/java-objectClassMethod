a26ede4895ab9bee90e71d38589170f67a645144
rla951214 <rla951214@naver.com>
2021-03-30 23:24:54 +0900

    'init'

---
+++ src/object/ToStringEx.java
@@ -0,0 +1,32 @@
+package object;
+
+class Book {
+	String title;
+	String author;
+	
+	Book(String title, String author){
+		this.title = title;
+		this.author = author;
+	}
+
+	@Override
+	public String toString() {
+		return title + "," + author;
+		
+	}
+	
+	
+}
+
+public class ToStringEx {
+
+	public static void main(String[] args) {
+		
+		Book book = new Book("자바공부", "김정태");
+		System.out.println(book);
+		
+		String str = new String("test");
+		System.out.println(str);
+	}
+
+}
